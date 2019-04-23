c139914b449af5c6f9bdba26c02e3188314bc5f9
ming <872394160@qq.com>
2019-04-23 18:10:31 +0800

    提交更新内容

--- staitc/js/hb.js
+++ staitc/js/hb.js
@@ -1,29 +1,129 @@
 "use strict";
-var Hb = /** @class */ (function () {
-    function Hb(options) {
-        this.options = options;
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var elWidth = document.body.offsetWidth;
+var elHeight = document.body.offsetHeight;
+var random = function (min, max) {
+    return Math.round(min + Math.random() * (max - min));
+};
+var Snows = /** @class */ (function () {
+    function Snows(options) {
+        this.options = {
+            images: "",
+            flakeCount: 35,
+            flakeColor: 'red',
+            flakePosition: 'absolute',
+            flakeIndex: 10000,
+            minSize: 50,
+            maxSize: 100,
+            minSpeed: 1,
+            maxSpeed: 5,
+        };
+        this.nowsArr = [];
+        this.options = __assign({}, this.options, options);
     }
-    /**
-     * start
-     */
-    Hb.prototype.start = function () {
-        console.log('tag', requestAnimationFrame);
-        this.clear();
+    Snows.prototype.inits = function () {
+        for (var i = 0; i < this.options.flakeCount; i += 1) {
+            var obj = {
+                _x: random(1, elWidth),
+                _y: random(1, elHeight),
+                _width: random(this.options.minSize, this.options.maxSize),
+                _height: random(this.options.minSize, this.options.maxSize),
+                _stepSize: random(1, 10) / 100,
+                _speed: random(this.options.minSpeed, this.options.maxSpeed)
+            };
+            var drops = new Drops(this.options, obj);
+            this.nowsArr.push(drops);
+        }
+    };
+    Snows.prototype.start = function () {
+        this.inits();
+        this.move();
+    };
+    Snows.prototype.move = function () {
+        var _this = this;
+        for (var _i = 0, _a = this.nowsArr; _i < _a.length; _i++) {
+            var item = _a[_i];
+            item.updata();
+        }
+        this.snowTimeout = requestAnimationFrame(function () {
+            _this.move();
+        });
     };
     /**
      * clear
      */
-    Hb.prototype.clear = function () {
+    Snows.prototype.clear = function () {
         var _this = this;
-        console.log(111);
         requestAnimationFrame(function () {
             _this.clear();
         });
     };
-    return Hb;
+    return Snows;
 }());
-var drops = /** @class */ (function () {
-    function drops(x, y) {
+var Drops = /** @class */ (function () {
+    function Drops(options, obj) {
+        this.step = 0;
+        this.options = options;
+        this.obj = obj;
+        this.init();
     }
-    return drops;
+    // private options:Object;
+    Drops.prototype.init = function () {
+        if (this.options.images) {
+            this.flake = document.createElement("img");
+            this.flake.src = this.options.images;
+        }
+        else {
+            this.flake = document.createElement("div");
+            this.flake.style.background = this.options.flakeColor;
+            this.flake.style.height = this.obj._height + 'px';
+        }
+        this.cssfun();
+        document.body.append(this.flake);
+        this.flake.addEventListener("click", this.change, false);
+    };
+    Drops.prototype.cssfun = function () {
+        this.flake.style.position = this.options.flakePosition;
+        this.flake.style.cursor = "pointer";
+        this.flake.className = "flake_snow";
+        this.flake.style.left = this.obj._x + 'px';
+        this.flake.style.top = this.obj._y + "px";
+        this.flake.style.width = this.obj._width + 'px';
+        this.flake.style.zIndex = this.options.flakeIndex;
+    };
+    Drops.prototype.updata = function () {
+        this.obj._y += this.obj._speed;
+        if (this.obj._y > elHeight - this.options.maxSize * 2 + this.options.minSize) {
+            this.reset();
+        }
+        this.flake.style.left = this.obj._x + 'px';
+        this.step += Number(this.obj._stepSize);
+        this.obj._x += Math.cos(this.step);
+        if (this.obj._x > (elWidth) - this.options.minSize || this.obj._x < 0) {
+            this.reset();
+        }
+        this.flake.style.top = this.obj._y + 'px';
+    };
+    Drops.prototype.reset = function () {
+        this.obj._y = 0;
+        this.obj._x = random(0, elWidth - this.options.maxSize);
+        this.obj._stepSize = random(1, 10) / 100;
+        this.obj._speed = random(this.options.minSpeed, this.options.maxSpeed);
+    };
+    Drops.prototype.change = function () {
+        if (window['SnowsClick']) {
+            var ins = window.SnowsClick();
+        }
+    };
+    return Drops;
 }());
--- staitc/js/hb.ts
+++ staitc/js/hb.ts
@@ -1,30 +1,133 @@
-class Hb {
-  constructor(options){
-     this.options=options
+
+
+let elWidth: number = document.body.offsetWidth
+let elHeight: number = document.body.offsetHeight
+let random = (min, max) => {
+  return Math.round(min + Math.random() * (max - min));
+};
+
+class Snows {
+  constructor(options) {
+    this.options = { ...this.options, ...options }
+
   }
-   private options:any
+
+  public options = {
+    images: "",
+    flakeCount: 35,
+    flakeColor: 'red',
+    flakePosition: 'absolute',
+    flakeIndex: 10000,
+    minSize: 50,
+    maxSize: 100,
+    minSpeed: 1,
+    maxSpeed: 5,
+
+  }
+
+  private nowsArr: Array<Drops> = []
+  private inits() {
+    for (let i = 0; i < this.options.flakeCount; i += 1) {
+      let obj = {
+        _x: random(1, elWidth),
+        _y: random(1, elHeight),
+        _width: random(this.options.minSize, this.options.maxSize),
+        _height: random(this.options.minSize, this.options.maxSize),
+        _stepSize: random(1, 10) / 100,
+        _speed: random(this.options.minSpeed, this.options.maxSpeed)
+      }
+      let drops = new Drops(this.options, obj)
+      this.nowsArr.push(drops)
+    }
+  }
   /**
    * start
    */
-  
+
+  private snowTimeout;
+
   public start() {
-    console.log('tag', requestAnimationFrame)
-     this.clear()
+    this.inits()
+    this.move()
+
   }
 
+  private move() {
+    for (let item of this.nowsArr) {
+      item.updata()
+    }
+    this.snowTimeout = requestAnimationFrame(() => {
+      this.move()
+    });
+  }
   /**
    * clear
    */
   public clear() {
-     console.log(111)
-     requestAnimationFrame(()=>{
+    requestAnimationFrame(() => {
       this.clear()
     })
   }
 }
 
-class  drops {
-  constructor(x,y) {
-     
+class Drops {
+  constructor(options, obj) {
+    this.options = options
+    this.obj = obj
+    this.init()
+  }
+  private flake
+  private obj;
+  private options
+  private step: number = 0
+  // private options:Object;
+  private init() {
+    if (this.options.images) {
+      this.flake = document.createElement("img");
+      this.flake.src = this.options.images
+    } else {
+      this.flake = document.createElement("div");
+      this.flake.style.background = this.options.flakeColor;
+      this.flake.style.height = this.obj._height + 'px'
+    }
+    this.cssfun()
+    document.body.append(this.flake)
+    this.flake.addEventListener("click", this.change, false);
+
+  }
+  private cssfun() {
+    this.flake.style.position = this.options.flakePosition;
+    this.flake.style.cursor = "pointer";
+    this.flake.className = "flake_snow";
+    this.flake.style.left = this.obj._x + 'px';
+    this.flake.style.top = this.obj._y + "px";
+    this.flake.style.width = this.obj._width + 'px';
+    this.flake.style.zIndex = this.options.flakeIndex;
+  }
+
+  updata() {
+    this.obj._y += this.obj._speed;
+    if (this.obj._y > elHeight - this.options.maxSize * 2 + this.options.minSize) {
+      this.reset()
+    }
+    this.flake.style.left = this.obj._x + 'px'
+    this.step += Number(this.obj._stepSize);
+    this.obj._x += Math.cos(this.step)
+    if (this.obj._x > (elWidth) - this.options.minSize || this.obj._x < 0) {
+      this.reset();
+    }
+    this.flake.style.top = this.obj._y + 'px'
+  }
+
+  private reset() {
+    this.obj._y = 0;
+    this.obj._x = random(0, elWidth - this.options.maxSize);
+    this.obj._stepSize = random(1, 10) / 100
+    this.obj._speed = random(this.options.minSpeed, this.options.maxSpeed);
+  }
+  private change() {
+    if (window['SnowsClick']) {
+      let ins: any = (<any>window).SnowsClick();
+    }
   }
 }
